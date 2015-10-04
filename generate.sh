36d98662cd72ea26899a0f7da075ce2ccf83325a
toastedcornflakes <toastedcornflakes@gmail.com>
2015-10-04 23:38:46 +0200

    Add a tweet this button in the footer

--- generate.sh
+++ generate.sh
@@ -14,13 +14,13 @@ for index in "${!articles[@]}"; do
 	# Spell check of the md file
 	aspell -c $i
 	# Please forgive me
-	(sed "s/page_html_title/$title/g" header.html; tail -n +2 $i | sed "s/^#/##/g" | python -m markdown -x codehilite ; cat footer.html) > $filename.html
+	(sed "s/page_html_title/$title/g" header.html; tail -n +2 $i | sed "s/^#/##/g" | python -m markdown -x codehilite ; cat footer_start.html; sed -e "s/post_tweet_title/$title/g" footer_addon_articles.html | sed -e "s/post_tweet_url/https%3A%2F%2Ftoastedcornflakes.github.io%2F/g"; cat footer_end.html) > $filename.html
 done
 
 # Generate the static pages
-(sed 's/page_html_title/Toasted corn flakes’ website/g' header.html; python -m markdown index.md; cat footer.html) > index.html 
-(sed 's/page_html_title/About me/' header.html; python -m markdown about.md; cat footer.html) > about.html 
- (sed 's/page_html_title//' header.html; python -m markdown 404.md; cat footer.html) > 404.html 
+(sed 's/page_html_title/Toasted corn flakes’ website/g' header.html; python -m markdown index.md; cat footer_start.html footer_end.html) > index.html 
+(sed 's/page_html_title/About me/' header.html; python -m markdown about.md; cat footer_start.html footer_end.html) > about.html 
+ (sed 's/page_html_title//' header.html; python -m markdown 404.md; cat footer_start.html footer_end.html) > 404.html 
 
 # generate CSS from templates
 cp base_style.css style.css
